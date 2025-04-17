34f09c65bcfa6b4ac0b0b6f3746c028da521039c
Gafarbek <abdusamatovgafarbek7@gmail.com>
2025-04-17 12:07:08 +0500

    Add files via upload

--- domBasicBid/script.js
+++ domBasicBid/script.js
@@ -2,10 +2,6 @@ const buttons = document.querySelector('.window button')
 const windows = document.querySelector('.windowBox')
 const exit = document.querySelector('a')
 
-buttons.addEventListener('click', () => {
+exit, buttons.addEventListener('click', () => {
     windows.style.display = 'flex'
-})
-
-exit.addEventListener('click', () => {
-    windows.style.display = 'none'
 })
